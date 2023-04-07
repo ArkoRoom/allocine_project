53954cd8e912243f92839108a8e9d22ab1e98e1b
Arkolips <catel.ludovic@gmail.com>
2023-04-07 11:45:59 +0200

    Ajout des appels Axios et création du composant VideoDetails

--- allocine/src/App.js
+++ allocine/src/App.js
@@ -1,11 +1,37 @@
+import React, {Component} from 'react'
 import MoviesContainer from "./containers/MoviesContainers";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <MoviesContainer />
-    </div>
-  );
+const API_END_POINT = "https://api.themoviedb.org/3/"
+const POPULAR_MOVIES_URL = "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images"
+const API_KEY = "api_key=2195faaed2128378c827a0ab3d051f06"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: {},
+      currentMovie: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then((response) => {
+        this.setState({movieList: response.data.results});
+        this.setState({currentMovie: response.data.results[0]});
+        console.log('AXIOS : ', response);
+        console.log('MOVIE LIST : ', this.state.movieList);
+        console.log('CURRENT MOVIE : ', this.state.currentMovie);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MoviesContainer />
+      </div>
+    )
+  }
 }
 
 export default App;
